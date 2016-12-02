14a2400997988b4401a26c97989cd56e935ad043
eestey <eestey@students.mchenry.edu>
2016-12-01 21:56:33 -0600

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-choice = 0
-while choice >= 0:
-    print "[======================]"
-    print "1. Returning Customer"
-    print "2. New Customer"
-    print "3. Guest"
-    print "[======================]\n"
-
-    choice = int(raw_input("Please select your choice: "))
