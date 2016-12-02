ecfbef9563e81999b7905ecebe22d87d1fff2b33
eestey <eestey@students.mchenry.edu>
2016-12-01 21:50:18 -0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+choice = 0
+while choice >= 0:
+    print "[======================]"
+    print "1. Returning Customer"
+    print "2. New Customer"
+    print "3. Guest"
+    print "[======================]\n"
+
+    choice = int(raw_input("Please select your choice: "))
